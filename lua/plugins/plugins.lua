28a1874cadee0f85e7cdee08bf8733d975808e4a
Javier de Marco <javierdemarcoo@gmail.com>
2021-12-14 20:11:02 +0100

    Added markdown, sintax and terminal plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,22 +1,22 @@
 -- File Containing the Plugins and their configuration
 -- Author: Javier de Marco
-
 return require('packer').startup(function(use)
 ------------
 -- Colors --
 -- ---------
     -- Colors in HEX
+    -- TODO: Auto enable colorizer
     use{
         'norcalli/nvim-colorizer.lua',
         config = function()
-            require('colorizer').setup{}
+            require('colorizer').setup()
         end,
     }
     -- Highlight range of operations
     use{
         'winston0410/range-highlight.nvim',
         config = function()
-            require('range-highlight').setup{}
+            require('range-highlight').setup()
         end,
         requires = { 'winston0410/cmd-parser.nvim'},
     }
@@ -37,33 +37,38 @@ return require('packer').startup(function(use)
 -- UI    --
 -----------
     -- UI enhancements for neovim
-    use{
-        'CosmicNvim/cosmic-ui',
-        config = function()
-            require('cosmic-ui').setup({
-                autocomplete = true
-            })
-        end,
-        requires = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim', 'ray-x/lsp_signature.nvim' },
-        after = {'nvim-lspconfig', 'nvim-cmp'}
-    }
+    -- TODO
+    --use{
+    --    'CosmicNvim/cosmic-ui',
+    --    config = function()
+    --        require('cosmic-ui').setup({
+    --           autocomplete = true
+    --        })
+    --    end,
+    --    requires = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim', 'ray-x/lsp_signature.nvim' },
+    --    after = {'nvim-lspconfig', 'nvim-cmp'}
+    --}
     -- Tab Bar
     use {
       'romgrk/barbar.nvim',
       requires = {'kyazdani42/nvim-web-devicons'}
     }
     -- Status line
-    use 'tamton-aquib/staline.nvim'
+    use{
+        'tamton-aquib/staline.nvim',
+        config = function()
+            require('staline').setup()
+        end,
+    }
     -- Cursor ui improvements
-    use 'yamatsum/nvim-cursorline.nvim'
+    use 'yamatsum/nvim-cursorline'
     -- Welcome screen in Neovim
+    -- TODO: Change title
     use {
         'glepnir/dashboard-nvim',
-        config = function()
-            require('dashboard-nvim').setupt({
-                dashboard_default_executive = 'telescope'
-            })
-        end,
+        config = {
+            'vim.g.dashboard_default_executive = "telescope"',
+        }
     }
     use "lukas-reineke/indent-blankline.nvim"
 ------------
@@ -74,7 +79,7 @@ return require('packer').startup(function(use)
         'neovim/nvim-lspconfig',
         config = function()
             -- Python
-            require('lspconfig').pyright.setup{}
+            require('lspconfig').pylsp.setup()
         end,
     }
     -- Colors for icons in errors and warnigns
@@ -85,5 +90,52 @@ return require('packer').startup(function(use)
     use 'simrat39/symbols-outline.nvim'
     -- LSP Installer
     use 'williamboman/nvim-lsp-installer'
+---------------
+-- MARKDOWN  --
+---------------
+    -- Live Preview
+    use 'ellisonleao/glow.nvim'
+    -- Markdown Workflow
+    use({'jakewvincent/mkdnflow.nvim',
+         config = function()
+            require('mkdnflow').setup({
+                -- Config goes here; leave blank for defaults
+            })
+         end
+    })
+    -- Preview in Browser
+    -- TODO: Doesnt work
+    use 'iamcco/markdown-preview.nvim'
+--------------
+-- SINTAX   --
+--------------
+    -- Better sintax highlight
+    use{
+        'nvim-treesitter/nvim-treesitter',
+        config = function()
+            require('nvim-treesitter').setup{
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                }
+            }
+        end,
+    }
+    -- Surrond operations
+    -- TODO: Doesnt work
+    use {
+        'blackCauldron7/surround.nvim',
+        config = function()
+            require('surround').setup{
+                mappings_style = "sandwich"
+            }
+        end,
+    }
+--------------
+-- Terminal --
+--------------
+    -- Terminal
+    use {"akinsho/toggleterm.nvim"}
+
 end)
 
